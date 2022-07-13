85393bd93397bfd608a54b38e2ea71a4f9609edc
gate-max <3chris@gmail.com>
2022-07-13 11:05:02 +0800

    Create Swiss.hs

---
+++ src/Swiss.hs
@@ -0,0 +1,9 @@
+
+-- | This module documents all the functions available in this package.
+--
+--   Most users should import the specific modules (e.g. @"Data.List.Swiss"@), which
+--   also reexport their non-@Extra@ modules (e.g. @"Data.List"@).
+
+module Swiss where
+
+import Data.List.Swiss
