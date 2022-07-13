a0c786f6bfd82d9240665d4e24202c7481fcee0f
gate-max <3chris@gmail.com>
2022-07-13 11:27:51 +0800

    Create Swiss.hs

---
+++ src/Data/List/Swiss.hs
@@ -0,0 +1,20 @@
+module Data.List.Extra(
+    module Data.List,
+    replace,
+    ) where
+    
+import Partial    
+import Data.List
+
+
+-- | Replace a subsequence everywhere it occurs. The first argument must
+--   not be the empty list.
+--
+-- > replace "el" "_" "Hello Kelly" == "H_lo K_ly"
+-- > replace "ll" "r" "Hello"       == "Hero"
+replace :: (Partial, Eq a) => [a] -> [a] -> [a] -> [a]
+replace [] _ _ = error "Swiss.replace, first argument cannot be empty"
+replace old new [] = []
+replace old new xs
+  | Just xs <- stripPrefix old xs = to ++ replace old new xs
+replace old new (x:xs) = x : replace old new xs
