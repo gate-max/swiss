0dc4af7a8035a038a6012bc047f347f53e4c753d
gate-max <3chris@gmail.com>
2022-07-13 11:37:20 +0800

    Update Swiss.hs

--- src/Data/List/Swiss.hs
+++ src/Data/List/Swiss.hs
@@ -16,5 +16,5 @@ replace :: (Partial, Eq a) => [a] -> [a] -> [a] -> [a]
 replace [] _ _ = error "Swiss.replace, first argument cannot be empty"
 replace old new [] = []
 replace old new xs
-  | Just xs <- stripPrefix old xs = to ++ replace old new xs
+    | Just xs <- stripPrefix old xs = to ++ replace old new xs
 replace old new (x:xs) = x : replace old new xs
